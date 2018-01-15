1173e94c05db76117772f104105c5a97fd6527ab
Pavan Boyapati <pavankumar.boyapati@gmail.com>
2018-01-15 13:56:55 -0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,26 @@
+ # import itertools
+
+# list = [1,2,3]
+
+# result = itertools.permutations(list, 2)
+# for r in result:
+# 	print (r)
+
+class Person:
+	"""docstring for Person"""
+	def __init__(self, name,age,salary):
+		# super(Person, self).__init__()
+		self.name = name
+		self.age = age
+		self.salary = salary
+
+p1 = Person('James',23,70000)
+
+l = ['james',23,80000]
+
+
+sentence = 'My name is {0[0]} and I am {0[1]} old with salary ${0[2]:,.2f}'.format(l)
+print(sentence)
+
+
+
